ce2756ecf6972f00bf5873fb9be3cbee85c698b4
niuweizhe <niu853273494@qq.com>
2021-02-06 22:31:13 +0800

    修复CI的编译错误

--- src/main/java/com/github/hcsp/Main.java
+++ src/main/java/com/github/hcsp/Main.java
@@ -47,7 +47,7 @@ public class Main {
         }
     }
 
-    private static Document httpGetAndParseHtml(String link) {
+    private static Document httpGetAndParseHtml(String link) throws IOException {
         CloseableHttpClient httpclient = HttpClients.createDefault();
 
         if (link.startsWith("//")) {
